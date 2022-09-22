12832a4e1b4f1d5ebb27952c31976656ea806544
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-09-21 21:29:19 -0700

    Puliendo

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# proyecto-1-TC
+# Proyecto 1 - Implementación de algoritmos básicos
+
+Teoría de la Computación - Sección 20
+
+Este proyecto consiste en la implementacion de algoritmos para la generacion de autómatas finitos a partir de expresiones regulares.
+
+Se implementaron los siguientes autómatas:
+- AFN - Automata Finito No Determinista 
+- AFD - Automata Finito Determinista 
+
+## Algoritmos Implementados
+
+- Transformación de expresión regular a postfix
+- Generacion de AFN
+- Conversion AFN a AFD
+- Generacion de AFD Directo
+- Minimización de AFD's
+- Simulación AFN y AFD
+
+## Librerias y Herramientas Necesarias 
+
+### `Graphviz`
+Descargar Graphviz: https://graphviz.org/download/
+Implementar el path: https://stackoverflow.com/questions/35064304/runtimeerror-make-sure-the-graphviz-executables-are-on-your-systems-path-aft
+
+Nos servirá para ver de una manera visual las simulaciones.
+
+### `Copy`
+Para copiar listas.
+
+### `Random`
+Para generar documentos PDF con los resultados obtenidos con nombres correlativos.
+
+### `Itertools`
+Para realizar combinaciones de elementos de una lista dada.
+
+## Autores
+
+- Esteban Cabrera - 17781
+- Elean Rivas - 19062
+- Cristian Laynez - 201281
--- main.py
+++ main.py
@@ -9,48 +9,43 @@ import traductorExpresion_a_AFD
 import minimizacion_afd
 import simulaciones
 
-### Se importa el modulo Nodo para utilizar la estructura definida para nodos
+# Se importa el modulo Nodo para utilizar la estructura definida para nodos
 from Nodo import Nodo
 
-### Iniciamos con las entradas del usuario
 pasar = False
 
 while not pasar:
-    ### Ingreso de la expresion
     expresion = input('Ingrese su expresion: ')
 
-    ### Ingreso de cadena de caracteres a evaluar
     cadena = input('Ingrese su input de caracteres a evaluar: ')
     cadena = cadena.replace(' ', '')
 
-    ### Se convierte la expresion regular a una estructura de arbol (listas agrupadas)
+    # Se convierte la expresion regular a una estructura de arbol (listas agrupadas)
     arbolExpresionRegular, pasar, mensaje = lectorExpresiones.conversionExpresionRegular('(' + expresion + ')')
-    ### Se convierte la expresion regular aumentada a una estructura de arbol (listas agrupadas)
+    # Se convierte la expresion regular aumentada a una estructura de arbol (listas agrupadas)
     arbolExpresionRegularAFD, pasar, mensaje = lectorExpresiones.conversionExpresionRegular('(' + expresion + ')#')
 
     if not pasar:
         print(mensaje)
 
-#print(arbolExpresionRegularAFD)
-
-###---------------------------------------------AFN---------------------------------------------###
-### Se convierten los nodos que no son operandos en Nodos para almacenar
-### Conjunto estados, transiciones, estado inicial, estado final
+#---------------------------------------------AFN---------------------------------------------#
+# Se convierten los nodos que no son operandos en Nodos para almacenar
+# Conjunto estados, transiciones, estado inicial, estado final
 arbolNodosExpresionRegular, correlativo = traductorExpresion_a_AFN.traduccionBase(arbolExpresionRegular, 0)
 
-### Uso de Thomson para generar un nodo final con el conjunto de estados, simbolos, transiciones, estado inicial y estados finales
+# Uso de Thomson para generar un nodo final con el conjunto de estados, simbolos, transiciones, estado inicial y estados finales
 afn, correlativo = traductorExpresion_a_AFN.traduccionAFN(arbolNodosExpresionRegular, correlativo, 0)
 
 
-### Simulacion AFN
+# Simulacion AFN
 resultadoAFN = simulaciones.simulacionAFN(afn, cadena)
 if resultadoAFN:
     print(f"AFN: La cadena de caracteres ingresada \'{cadena}\' SI es parte del lenguaje generado por la expresion \'{expresion}\'")
 else:
     print(f"AFN: La cadena de caracteres ingresada \'{cadena}\' NO es parte del lenguaje generado por la expresion \'{expresion}\'")
 
-### Generar diagrama AFN
-f = Digraph('Automata Finito No Determinista', filename='AFN'+str(random.random()))
+# Generar diagrama AFN
+f = Digraph('Automata Finito No Determinista', filename='resultados/AFN'+str(random.random()))
 f.attr(rankdir='LR', size='8,5')
 
 f.attr('node', shape='doublecircle')
@@ -72,23 +67,23 @@ for transicion in afn.transiciones:
 
 f.view()
 
-###---------------------------------------------AFD---------------------------------------------###
-### Creacion de subconjuntos para pasar de AFN -> AFD 
+#---------------------------------------------AFD---------------------------------------------#
+# Creacion de subconjuntos para pasar de AFN -> AFD 
 dStates, dTrans = afn_a_afd.traduccionAFD(afn)
 
-### Creamos una estructura de Nodo para simular el AFD
-afd = afn_a_afd.convertirAFDNodo(afn, dStates, dTrans)
+# Creamos una estructura de Nodo para simular el AFD
+afd = afn_a_afd.convert_afd_nodo(afn, dStates, dTrans)
 
 
-### Simulacion AFD
+# Simulacion AFD
 resultadoAFD = simulaciones.simulacionAFD(afd, cadena)
 if resultadoAFD:
     print(f"AFD: La cadena de caracteres ingresada \'{cadena}\' SI es parte del lenguaje generado por la expresion \'{expresion}\'")
 else:
     print(f"AFD: La cadena de caracteres ingresada \'{cadena}\' NO es parte del lenguaje generado por la expresion \'{expresion}\'")
 
-### Generar diagrama AFD
-f = Digraph('Automata Finito Determinista', filename='AFD'+str(random.random()))
+# Generar diagrama AFD
+f = Digraph('Automata Finito Determinista', filename='resultados/AFD'+str(random.random()))
 f.attr(rankdir='LR', size='8,5')
 
 f.attr('node', shape='doublecircle')
@@ -110,48 +105,48 @@ for transicion in afd.transiciones:
 
 f.view()
 
-###------------------------------------------AFD-DIRECTO----------------------------------------###
-### Se hace una sustitucion previa para las expresiones 
+#------------------------------------------AFD-DIRECTO----------------------------------------#
+# Se hace una sustitucion previa para las expresiones 
 arbolNodosExpresionRegularSustituido = traductorExpresion_a_AFD.sustitucionPrevia(arbolExpresionRegularAFD)
 
 arbolNodosExpresionRegularAFD, _, correspondencias = traductorExpresion_a_AFD.traduccionBase(arbolNodosExpresionRegularSustituido, 1, [])
 
-### Obtenemos los nodos hojas que ya poseen sus posiciones
+# Obtenemos los nodos hojas que ya poseen sus posiciones
 nodosHoja = traductorExpresion_a_AFD.devolverNodosHoja(arbolNodosExpresionRegularAFD, [])
 
-### Se realiza la definicion de nodos que no son hojas con sus operaciones nullable, firstpos, lastpos
+# Se realiza la definicion de nodos que no son hojas con sus operaciones nullable, firstpos, lastpos
 nodoRoot, nodos = traductorExpresion_a_AFD.definirNodosAFD(arbolNodosExpresionRegularAFD, 0, [])
 
-### Unimos los nodos en un solo arreglo
+# Unimos los nodos en un solo arreglo
 nodosFinales = nodosHoja + nodos
 
 
-### Se calcula la tabla de followpos con los nodosFinales resultantes
+# Se calcula la tabla de followpos con los nodosFinales resultantes
 tablaFollowpos = traductorExpresion_a_AFD.followpos(nodosFinales, correspondencias)
 
 
-### Obtener el conjunto de simbolos
+# Obtener el conjunto de simbolos
 simbolos = traductorExpresion_a_AFD.simbolosAFDDirecta(correspondencias)
 
-### Obtener las transiciones y estados (el primer estado es el estado inicial)
+# Obtener las transiciones y estados (el primer estado es el estado inicial)
 dStatesAFD, dTransAFD  = traductorExpresion_a_AFD.traduccionAFDDirecta(nodoRoot, simbolos, tablaFollowpos, correspondencias)
 
-### Posicion para determinar que estados son finales
+# Posicion para determinar que estados son finales
 posicionFinal = correspondencias[-1][1]
 
-### Creamos una estructura de Nodo para simular el AFD
+# Creamos una estructura de Nodo para simular el AFD
 afdd = traductorExpresion_a_AFD.convertirAFDDirectaNodo(dStatesAFD, dTransAFD, simbolos, posicionFinal)
 
 
-### Simulacion AFD Directa
+# Simulacion AFD Directa
 resultadoAFDD = simulaciones.simulacionAFD(afdd, cadena)
 if resultadoAFDD:
     print(f"AFD Directa: La cadena de caracteres ingresada \'{cadena}\' SI es parte del lenguaje generado por la expresion \'{expresion}\'")
 else:
     print(f"AFD Directa: La cadena de caracteres ingresada \'{cadena}\' NO es parte del lenguaje generado por la expresion \'{expresion}\'")
 
-### Generar diagrama AFD
-f = Digraph('Automata Finito Determinista (Directa)', filename='AFD_Directa'+str(random.random()))
+# Generar diagrama AFD
+f = Digraph('Automata Finito Determinista (Directa)', filename='resultados/AFD_Directa'+str(random.random()))
 f.attr(rankdir='LR', size='8,5')
 
 f.attr('node', shape='doublecircle')
@@ -173,22 +168,22 @@ for transicion in afdd.transiciones:
 
 f.view()
 
-###------------------------------------------MINIMIZACION-AFD---------------------------------------###
-### Minimizar el AFD generado a partir del AFN
+#------------------------------------------MINIMIZACION-AFD---------------------------------------#
+# Minimizar el AFD generado a partir del AFN
 afdmStates = minimizacion_afd.traduccionAFDMinimizado(afd)
 
-### Ordenar el AFD minimizado con la estructura de Nodos
+# Ordenar el AFD minimizado con la estructura de Nodos
 afdm = minimizacion_afd.convertirAFDMinimizadoNodo(afdmStates, afd)
 
-### Simulacion AFD Minimizado
+# Simulacion AFD Minimizado
 resultadoAFDM = simulaciones.simulacionAFD(afdm, cadena)
 if resultadoAFDM:
     print(f"AFD Minimizado: La cadena de caracteres ingresada \'{cadena}\' SI es parte del lenguaje generado por la expresion \'{expresion}\'")
 else:
     print(f"AFD Minimizado: La cadena de caracteres ingresada \'{cadena}\' NO es parte del lenguaje generado por la expresion \'{expresion}\'")
 
-### Generar diagrama AFD Minimizado
-f = Digraph('Automata Finito Determinista (Minimizado)', filename='AFD_Minimizado'+str(random.random()))
+# Generar diagrama AFD Minimizado
+f = Digraph('Automata Finito Determinista (Minimizado)', filename='resultados/AFD_Minimizado'+str(random.random()))
 f.attr(rankdir='LR', size='8,5')
 
 f.attr('node', shape='doublecircle')
